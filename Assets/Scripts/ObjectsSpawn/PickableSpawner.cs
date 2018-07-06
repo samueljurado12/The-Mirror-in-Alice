3d8097f65837db29883dacbb9d6a3eadfe5d9102
Samuel Jurado Quintana <samueljurado12@gmail.com>
2018-07-06 21:07:50 +0200

    Pickable objects spawn
    Spawner controller and spawning points scripts

---
+++ Assets/Scripts/ObjectsSpawn/PickableSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableSpawner : MonoBehaviour {
+
+    [SerializeField]
+    private float maxSpawns;
+
+    private List<ObjectSpawnPoint> possibleSpawnPoints = new List<ObjectSpawnPoint>();
+
+
+	// Use this for initialization
+	void Start () {
+        foreach(ObjectSpawnPoint spawnPoint in GetComponentsInChildren<ObjectSpawnPoint>()) {
+            possibleSpawnPoints.Add(spawnPoint);
+        }
+
+        for(int i = 0; i < maxSpawns; i++) {
+            ObjectSpawnPoint selected = pickRandom(possibleSpawnPoints);
+            possibleSpawnPoints.Remove(selected);            
+            selected.spawn();
+        }
+        
+	}
+
+    private ObjectSpawnPoint pickRandom(List<ObjectSpawnPoint> possibleSpawnPoints) {
+        int index = Random.Range(0, possibleSpawnPoints.ToArray().Length);
+        return (ObjectSpawnPoint)possibleSpawnPoints.ToArray()[index];
+    }
+}
