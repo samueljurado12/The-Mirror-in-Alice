ca8f13d0d7ecb387bf5f27cf31e839ef83daa597
albertosanchezbazan <alsaba94@hotmail.com>
2018-07-06 23:32:37 +0200

    +Target, +ScoreService, +PlayerCollisionBehaviour

---
+++ Assets/PlayerCollisionBehaviour.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionBehaviour : MonoBehaviour
+{
+
+    public bool isCatcher;
+    public bool isPlayerOne;
+    ScoreService scoreService;
+
+    // Use this for initialization
+    void Start()
+    {
+        scoreService = ScoreService.getInstance();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("target"))
+        {
+            Target target = collision.gameObject.GetComponent<Target>();
+
+            if (target.isCatchable == isCatcher)
+            {
+                // Sumar puntuaciones
+                if (isPlayerOne)
+                {
+                    scoreService.increasePlayer1Score(target.score);
+                }
+                else
+                {
+                    scoreService.increasePlayer2Score(target.score);
+                }
+
+            }
+            else
+            {
+                // Restar puntuación y cambiar modo
+                if (isPlayerOne)
+                {
+                    scoreService.decreasePlayer1Score(target.score);
+                }
+                else
+                {
+                    scoreService.decreasePlayer2Score(target.score);
+                }
+
+                isCatcher = !isCatcher;
+            }
+
+            Destroy(collision.gameObject);
+        }
+    }
+}
