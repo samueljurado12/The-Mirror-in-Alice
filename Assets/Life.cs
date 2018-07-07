98979decaf4a6327a97d14aaf5adb132a80863d3
Alvaro de Marcos <almaral4c@gmail.com>
2018-07-08 01:19:52 +0200

    Life UI Works

---
+++ Assets/Life.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Life : MonoBehaviour {
+
+	public Sprite sprite;
+
+	public SpriteRenderer spriteRenderer;
+
+	public void SwapSprites () {
+		spriteRenderer.sprite = sprite;
+	}
+}
--- Assets/Scripts/Services/LifeService.cs
+++ Assets/Scripts/Services/LifeService.cs
@@ -2,53 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LifeService {
+public class LifeService : MonoBehaviour {
 
-    public int player1Life, player2Life;
+	public int player1Life, player2Life;
 
-    public LifeService() {
-        player1Life = 3; //TODO: Use settings 
-        player2Life = 3; //TODO: Use settings
-    }
+	public Life vida1Player1;
+	public Life vida2Player1;
+	public Life vida3Player1;
+	public Life vida1Player2;
+	public Life vida2Player2;
+	public Life vida3Player2;
 
-    static LifeService instance;
 
-    internal static LifeService getInstance()
-    {
-        if (instance == null)
-        {
-            instance = new LifeService();
-        }
-        return instance;
-    }
+	void Start () {
+		player1Life = 3; //TODO: Use settings 
+		player2Life = 3; //TODO: Use settings
+	}
 
-    public void player1GainsLife() {
-        player1Life++;
-    }
+	static LifeService instance;
 
-    public void player2GainsLife()
-    {
-        player2Life++;
-    }
+	internal static LifeService getInstance () {
+		if (instance == null) {
+			instance = new LifeService ();
+		}
+		return instance;
+	}
 
-    public bool player1LosesLife()
-    {
-        bool playerHasZeroLives = false;
-        player1Life--;
-        if (player1Life == 0) {
-            playerHasZeroLives = true;
-        }
-        return playerHasZeroLives;
-    }
+	public void player1GainsLife () {
+		player1Life++;
+	}
 
-    public bool player2LosesLife()
-    {
-        bool playerHasZeroLives = false;
-        player2Life--;
-        if (player2Life == 0)
-        {
-            playerHasZeroLives = true;
-        }
-        return playerHasZeroLives;
-    }
+	public void player2GainsLife () {
+		player2Life++;
+	}
+
+	public bool player1LosesLife () {
+		bool playerHasZeroLives = false;
+		player1Life--;
+		if (player1Life == 0) {
+			playerHasZeroLives = true;
+			vida1Player1.SwapSprites ();
+		} else if (player1Life == 1) {
+			vida2Player1.SwapSprites ();
+		} else if (player1Life == 2) {
+			vida3Player1.SwapSprites ();
+		}
+		return playerHasZeroLives;
+	}
+
+	public bool player2LosesLife () {
+		bool playerHasZeroLives = false;
+		player2Life--;
+		if (player2Life == 0) {
+			playerHasZeroLives = true;
+			vida1Player2.SwapSprites ();
+		} else if (player1Life == 1) {
+			vida2Player2.SwapSprites ();
+		} else if (player1Life == 2) {
+			vida3Player2.SwapSprites ();
+		}
+		return playerHasZeroLives;
+	}
 }
