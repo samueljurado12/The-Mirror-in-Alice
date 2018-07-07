394f2a7b2ee84ddaf68876c45c5030bb00e1c9b9
Samuel Jurado Quintana <samueljurado12@gmail.com>
2018-07-08 01:15:45 +0200

    Updated music fadin in main menu

--- Assets/MenuMusic.cs
+++ Assets/MenuMusic.cs
@@ -6,7 +6,7 @@ public class MenuMusic : MonoBehaviour {
 
     // Use this for initialization
     void Start() {
-        StartCoroutine(MusicFader.fadeIn(GetComponent<AudioSource>(), 1f, 1f));
+        StartCoroutine(MusicFader.fadeIn(GetComponent<AudioSource>(), 2f, 1f));
     }
 
 }
