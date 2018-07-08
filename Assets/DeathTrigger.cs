6e600c31cac9cf18bf2620f9d176612650d6b0dc
Alvaro de Marcos <almaral4c@gmail.com>
2018-07-08 13:03:20 +0200

    Added Triggers..

--- Assets/DeathTrigger.cs
+++ Assets/DeathTrigger.cs
@@ -6,8 +6,13 @@ using UnityEngine.SceneManagement;
 public class DeathTrigger : MonoBehaviour {
 
 	void OnTriggerEnter2D (Collider2D col) {
-		Debug.Log ("muerto");
-		//SceneManager.LoadScene ("");
+
+		if (col.gameObject.CompareTag ("Player1")) {
+			SceneManager.LoadScene ("GanarFalsa");
+		} else {
+			SceneManager.LoadScene ("GanarReal");
+		}
+
 
 	}
 }
