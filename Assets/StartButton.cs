3d4bfdefb27f06d08304b1da4a36401bcc706cb1
Samuel Jurado Quintana <samueljurado12@gmail.com>
2018-07-08 10:20:32 +0200

    Start Button animation

---
+++ Assets/StartButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButton : MonoBehaviour {
+
+    [SerializeField]
+    private LevelManager lvlmngr;
+
+	public void loadGameSelector() {
+        lvlmngr.LoadScene("01b_GameModeSelector");
+    }
+}
